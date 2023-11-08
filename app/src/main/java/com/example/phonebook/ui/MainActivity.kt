cdd336ae7c137225f363c104a6b414dd56700423
Fábio de Carvalho <fdc1994@hotmail.com>
2023-11-08 11:41:40 +0100

    add compose UI

--- app/src/main/java/com/example/phonebook/ui/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/ui/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.phonebook.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.phonebook.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.example.phonebook.data.ContactDatabase
+import com.example.phonebook.ui.theme.PhoneBookTheme
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ContactDatabase::class.java,
+            "contacts.db"
+        ).build()
+    }
+
+    private val viewModel.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            PhoneBookTheme {
+              ContactScreen(state = , onEvent = )
+            }
+        }
     }
 }
